61c11a0ecee71b386f00a76b7c3d749bf509f39f
Chloé <chloe.larroze@etu.emse.fr>
2025-10-12 22:42:32 +0200

    feat : SolutionEvaluator class TODO

--- utils/SolutionEvaluator.java
+++ utils/SolutionEvaluator.java
@@ -1,5 +1,11 @@
 package utils;
 
+import solution.*;
+
 public class SolutionEvaluator {
+    public static int evaluate(Solution solution) {
+        // TODO 
+        return solution.getMakespan(); // Exemple : retourne le makespan de la solution
+    }
     
 }
